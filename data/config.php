b3f3aa0239043af18be09173b9d35bf6dabb00db
luchongchong <root@iZ234jt82r8Z.(none)>
2017-04-11 13:31:43 +0800

    fix bug data

---
+++ data/config.php
@@ -0,0 +1,48 @@
+<?php
+// database host
+$db_host   = "127.0.0.1:3306";
+
+// database name
+$db_name   = "MeifangDB";
+
+// database username
+$db_user   = "user_meifang";
+
+// database password
+$db_pass   = "user_meifang#2016";
+
+// table prefix
+$prefix    = "ecs_";
+
+$timezone    = "Asia/Shanghai";
+
+$cookie_path    = "/";
+
+$cookie_domain    = "";
+
+$session = "1440";
+
+define('EC_CHARSET','utf-8');
+
+define('ADMIN_PATH','admin');
+
+define('CATEGROY_NAME_PLACE', '儿童房');
+
+define('CATEGROY_NAME_STYLE', '简约');
+
+define('CATEGROY_PLACE', '场所');
+define('CATEGROY_STYLE', '风格');
+define('CATEGROY_MATERIAL', '材质');
+
+
+define('SITE_URL','http://test.mfmb58.com/');
+
+//设置评论的类型
+define('COMMENT_TYPE_DIANMIAN', 3);
+
+define('AUTH_KEY', 'this is a key');
+
+define('OLD_AUTH_KEY', '');
+define('API_TIME', '2017-04-08 13:48:20');
+
+?>
---
+++ data/flashdata/pinkfocus/data.js
@@ -0,0 +1,16 @@
+imgUrl1="data/afficheimg/20150205rknpgt.jpg";
+imgtext1="";
+imgLink1=escape("http://www.68ecshop.com/ecshop_topic/xjd/");
+imgUrl2="data/afficheimg/20150205avwlqh.jpg";
+imgtext2="";
+imgLink2=escape("http://www.68ecshop.com/ecshop_topic/brand/");
+imgUrl3="data/afficheimg/20150205bhrtbe.jpg";
+imgtext3="";
+imgLink3=escape("http://www.68ecshop.com/ecshop_topic/aliy/");
+imgUrl4="data/afficheimg/20150205wnqtsq.jpg";
+imgtext4="";
+imgLink4=escape("http://www.68ecshop.com/ecshop_topic/appv3/");
+
+var pics=imgUrl1+"|"+imgUrl2+"|"+imgUrl3+"|"+imgUrl4;
+var links=imgLink1+"|"+imgLink2+"|"+imgLink3+"|"+imgLink4;
+var texts=imgtext1+"|"+imgtext2+"|"+imgtext3+"|"+imgtext4;
